e0de39670b71369a07c0a09585acc720839d5594
Rodriguez Perez Jonathan CB128 <91566167+RodriguezPerezJonathanCB128@users.noreply.github.com>
2021-11-03 14:41:15 -0700

    Add files via upload

---
+++ DAWeb_Act8_Rodriguez/Materias.php
@@ -0,0 +1,33 @@
+<?php include('header.php'); ?>
+
+<nav class="navbar">
+    <li><a href="Index.php">Nosotros</a></li> 
+    <li><a href="Alumno.php">Alumno</a></li>
+    <li><a href="Materias.php">Materias</a></li>
+</nav>
+
+  <?php include('sidebar.php'); ?>
+
+  <section class="section">
+  <article class="main"><h1><p>-Contamos con distintas especialidades para el Alumno.</p>
+  <p>Las especialidades son las siguientes:</p>
+  <p>-Administracion de Recursos Humanos</p>
+  <p>-Gericultura</p>
+  <p>-Programacion</p>
+  <p>-Laboratorista Quimico</p>
+  <p>-Electronica</p>
+  <p>-Refrigeracion y Climatizacion</p>
+  <p>-Soporte y mantenimiento de equipo de computo</p>
+
+
+</h1>
+<br><a href="#"><img src="img/papita.jpg" height="300px" width="700px"></br>
+</article>
+  </section>
+
+ <?php include('footer.php'); ?>
+
+  Rodriguez Perez Jonathan 5-J
+
+</body>
+</html>
---
+++ DAWeb_Act8_Rodriguez/sidebar.php
@@ -0,0 +1,7 @@
+<aside class="sidebar"> 
+      <br>-Noticias</br>
+      <br>-Blog</br>
+      <br>-Correo</br>
+      <br>-Horarios</br>
+      <br><a href="#"><img src="img/logo.jpg" height="170px" width="170px"></a></br>
+  </aside>
